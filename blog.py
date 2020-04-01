04127c55d3b8560ee6372e5023861bf37b347ab5
mau5mat <mau5mat@gmail.com>
2020-04-01 16:03:55 +0100

    first commit

---
+++ blog.py
@@ -0,0 +1,77 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from forms import RegistrationForm, LoginForm
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b99b0bea3dbdf9fd26ed064906b668e5'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+database = SQLAlchemy(app)
+
+
+class User(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    username = database.Column(database.String(20), unique=True, nullable=False)
+    email = database.Column(database.String(120), unique=True, nullable=False)
+    image_file = database.Column(database.String(20), nullable=False, default="default.jpeg")
+    password = database.Column(database.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+    
+
+
+posts = [
+    {
+        "author": "Isabelle",
+        "title": "Blog Post 666",
+        "content": "How I learned to love the Chainsaw",
+        "date_posted": "March 21, 2020"
+    },
+    {
+        "author": "Tom Nook",
+        "title": "Blog Post 2",
+        "content": "Bells, stop ringing 'em and start making 'em",
+        "date_posted": "March 23, 2020"
+    }
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("home.html", posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html", title="about")
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f"Account created: {form.username.data}!", category="success")
+        return redirect(url_for("home"))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", category="success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful, please check your credentials", "danger")
+
+    return render_template('login.html', title='Login', form=form)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ forms.py
@@ -0,0 +1,21 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, length, Email, EqualTo
+
+# Registration Form
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                            validators=[DataRequired(), length(min=2, max=20)])
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+# Log-in Form
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log-in')
